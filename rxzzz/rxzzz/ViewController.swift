b6f50ec5a5550dd6fb170407ce4cccfdc224b124
姜守栋 <18511632756@163.com>
2018-01-16 16:26:03 +0800

    响应式编程样例

--- rxzzz/rxzzz/ViewController.swift
+++ rxzzz/rxzzz/ViewController.swift
@@ -7,19 +7,47 @@
 //
 
 import UIKit
-
+import RxCocoa
+import RxSwift
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    let musicViewModel = MusicViewModel()
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        musicViewModel.data.bind(to: tableView.rx.items(cellIdentifier: "musicCell")) { _, music, cell in
+            cell.textLabel?.text = music.name
+            cell.detailTextLabel?.text = music.singer
+        }.disposed(by: disposeBag)
+        tableView.rx.modelSelected(Music.self).subscribe(onNext: { (music) in
+            print("你选的歌曲信息【\(music)】")
+        }).disposed(by: disposeBag)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+struct MusicViewModel {
+    let data = Observable.just([
+        Music(name: "无条件", singer: "陈奕迅"),
+        Music(name: "你曾是少年", singer: "S.H.E"),
+        Music(name: "从前的我", singer: "陈洁仪"),
+        Music(name: "在木星", singer: "朴树")
+        ])
+}
+
+struct Music {
+    let name: String
+    let singer: String
+    
+    init(name: String, singer: String) {
+        self.name = name
+        self.singer = singer
+    }
+}
+
+extension Music: CustomStringConvertible {
+    var description: String {
+        return "name: \(name) singer: \(singer)"
+    }
+}
